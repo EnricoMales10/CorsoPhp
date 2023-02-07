d28830b86371c5190eff86cbca1049cd60239c58
Enrico Malesani <enrico.malesani@hotmail.it>
2023-02-07 09:14:06 +0100

    Post/Get

--- form_in_php/log-in-response.php
+++ form_in_php/log-in-response.php
@@ -1,9 +1,23 @@
-<h1>
+<?php
+/*<h1>
     Sono la risposta (RESPONSE)
 </h1>
-<?php
+
 echo "<pre>";
+echo "get:";
 print_r($_GET);
+echo "post:";
+print_r($_POST);
 echo "</pre>";
 
+echo "La tua email è <br>";
+echo "<strong>" . $_POST["email"] . "</strong>";*/
+
+$test = filter_input(INPUT_GET,"email",FILTER_VALIDATE_EMAIL);
+
+if ($test === false) {
+    echo "\nla mail non è valida\n";
+} else {
+    echo "grazie la tua email è valida: $test";
+}
 ?>
