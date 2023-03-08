114c6c5d89e4afe96ea9b0b4fa120ced922afe3a
Enrico Malesani <enrico.malesani@hotmail.it>
2023-03-08 09:37:37 +0100

    inizio esercizio province

--- basi_json/install.sql
+++ basi_json/install.sql
@@ -15,3 +15,15 @@ VALUES('Abruzzo');
 SELECT * FROM regione;
 
 TRUNCATE TABLE regione;
+
+CREATE TABLE Provincia (
+    id_provincia int NOT NULL AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    sigla char(2) NOT NULL,
+    id_regione int,
+    PRIMARY KEY (id_provincia)
+);
+
+SELECT * FROM Provincia;
+
+TRUNCATE TABLE Provincia;
